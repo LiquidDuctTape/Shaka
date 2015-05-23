a431539a5a3275ccb4c1ab02840e1526365f8a09
Spencer Lanman <jimmie3755@yahoo.com>
2015-05-23 13:27:35 -0400

    Initial Commit

---
+++ src/shaka/Shaka.java
@@ -0,0 +1,38 @@
+package shaka;
+
+import java.io.File;
+import org.lwjgl.LWJGLUtil;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ *
+ * @author Spencer
+ */
+public class Shaka extends StateBasedGame {
+
+	public Shaka() {
+		super("Shaka");
+	}
+	
+	public static void main(String[] args) {
+		System.setProperty("org.lwjgl.librarypath", new File(new File(System.getProperty("user.dir"), "natives"), LWJGLUtil.getPlatformName()).getAbsolutePath());
+		System.setProperty("net.java.games.input.libarypath", System.getProperty("org.lwjgl.librarypath"));
+
+		try {
+			AppGameContainer app = new AppGameContainer(new Shaka());
+			app.setDisplayMode(800, 600, false);
+			app.setTargetFrameRate(60);
+			app.setShowFPS(false);
+			app.start();
+		} catch (SlickException e) {
+		}
+	}
+
+	@Override
+	public void initStatesList(GameContainer gc) throws SlickException {
+		
+	}
+}
