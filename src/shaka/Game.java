dfc7da7d3a636e1206fc341be91df6ddfda769d9
Spencer Lanman <jimmie3755@yahoo.com>
2015-05-23 20:00:35 -0400

    Implemented background and shaka

---
+++ src/shaka/Game.java
@@ -0,0 +1,175 @@
+
+package shaka;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.GameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Game implements GameState {
+
+	private Image background, shaka;
+	private float x, y;
+	
+	@Override
+	public int getID() {
+		return ShakaMap.GAMEID;
+	}
+
+	@Override
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		background = new Image(ShakaMap.BACKGROUNDIMAGE);
+		shaka = new Image(ShakaMap.SHAKAIMAGE);
+		x = gc.getWidth() / 2;
+		y = gc.getHeight() / 2;
+	}
+
+	@Override
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
+		background.draw();
+		shaka.draw(x, y);
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+		if (gc.getInput().isKeyPressed('W')) {
+			y -= delta * 5;
+		}
+		
+		if (gc.getInput().isKeyPressed('S')) {
+			y += delta * 5;
+		}
+		
+		if (gc.getInput().isKeyPressed('A')) {
+			x -= delta * 5;
+		}
+		
+		if (gc.getInput().isKeyPressed('D')) {
+			x += delta * 5;
+		}
+	}
+
+	@Override
+	public void enter(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		
+	}
+
+	@Override
+	public void leave(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		
+	}
+
+	@Override
+	public void mouseWheelMoved(int i) {
+		
+	}
+
+	@Override
+	public void mouseClicked(int i, int i1, int i2, int i3) {
+		
+	}
+
+	@Override
+	public void mousePressed(int i, int i1, int i2) {
+		
+	}
+
+	@Override
+	public void mouseReleased(int i, int i1, int i2) {
+		
+	}
+
+	@Override
+	public void mouseMoved(int i, int i1, int i2, int i3) {
+		
+	}
+
+	@Override
+	public void mouseDragged(int i, int i1, int i2, int i3) {
+		
+	}
+
+	@Override
+	public void setInput(Input input) {
+		
+	}
+
+	@Override
+	public boolean isAcceptingInput() {
+		return true;
+	}
+
+	@Override
+	public void inputEnded() {
+		
+	}
+
+	@Override
+	public void inputStarted() {
+		
+	}
+
+	@Override
+	public void keyPressed(int i, char c) {
+		
+	}
+
+	@Override
+	public void keyReleased(int i, char c) {
+		
+	}
+
+	@Override
+	public void controllerLeftPressed(int i) {
+		
+	}
+
+	@Override
+	public void controllerLeftReleased(int i) {
+		
+	}
+
+	@Override
+	public void controllerRightPressed(int i) {
+		
+	}
+
+	@Override
+	public void controllerRightReleased(int i) {
+		
+	}
+
+	@Override
+	public void controllerUpPressed(int i) {
+		
+	}
+
+	@Override
+	public void controllerUpReleased(int i) {
+		
+	}
+
+	@Override
+	public void controllerDownPressed(int i) {
+		
+	}
+
+	@Override
+	public void controllerDownReleased(int i) {
+		
+	}
+
+	@Override
+	public void controllerButtonPressed(int i, int i1) {
+		
+	}
+
+	@Override
+	public void controllerButtonReleased(int i, int i1) {
+		
+	}
+	
+}
---
+++ src/shaka/ShakaMap.java
@@ -0,0 +1,9 @@
+
+package shaka;
+
+public class ShakaMap {
+	
+	public static final int GAMEID = 0;
+	
+	public static final String BACKGROUNDIMAGE = "", SHAKAIMAGE = "";
+}
